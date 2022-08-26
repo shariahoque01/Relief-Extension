f9a9fb0c8d2c08c0cdb4c0d1690bb7eb439859df
shariahoque01 <77020066+shariahoque01@users.noreply.github.com>
2022-08-26 00:09:46 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,36 @@
+chrome.alarms.onAlarm.addListener(
+    () => {
+        chrome.notifications.create(
+            // "Relax_time",
+            {
+                type: "basic",
+                iconUrl: "icon_128.png",
+                title: "Take a break",
+                message: "Have a sip of water human!",
+                silent: false
+            },
+            () => { }
+        )
+    },
+)
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(request);
+        if (request.time)
+            createAlarm();
 
+        sendResponse(() => {
+            return false
+        });
+    }
+);
+
+function createAlarm() {
+    chrome.alarms.create(
+        "Relax_time",
+        {
+            delayInMinutes: 1,
+            periodInMinutes: 1
+        }
+    );
+}
